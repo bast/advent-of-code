b32ed218d83c27b22cb8e15255bc42f6575a9163
Radovan Bast <bast@users.noreply.github.com>
2020-12-19 19:55:56 +0100

    day 17, part 2

--- 2020/day-17/src/main.rs
+++ 2020/day-17/src/main.rs
@@ -1,101 +1,139 @@
 use std::collections::HashSet;
 use std::fs;
 
-#[derive(PartialEq, Eq, Hash, Copy, Clone, Debug)]
-struct Position {
-    x: i32,
-    y: i32,
-    z: i32,
-}
+type Position = Vec<i32>;
 
 fn main() {
     let input = fs::read_to_string("input.txt").unwrap();
 
-    let mut active_cells = read_input(&input);
-
-    for _ in 0..6 {
-        active_cells = timestep(&active_cells);
-    }
-
-    println!("number of active cells (part 1): {}", active_cells.len());
+    println!("number of active cells (part 1): {}", driver(&input, 6, 3));
+    println!("number of active cells (part 2): {}", driver(&input, 6, 4));
 }
 
-fn timestep(active_cells: &HashSet<Position>) -> HashSet<Position> {
+fn driver(input: &str, num_steps: usize, num_dimensions: usize) -> usize {
+    let mut active_cells = read_input(&input, num_dimensions);
+
+    for _ in 0..num_steps {
+        active_cells = timestep(&active_cells, num_dimensions);
+    }
+
+    active_cells.len()
+}
+
+fn timestep(active_cells: &HashSet<Position>, num_dimensions: usize) -> HashSet<Position> {
     let mut new_cells = HashSet::new();
 
     // If a cube is active and exactly 2 or 3 of its neighbors are also active, the cube remains
     // active. Otherwise, the cube becomes inactive.
-    for &cell in active_cells {
-        let n = num_active_neighbors(&cell, &active_cells);
+    for cell in active_cells {
+        let n = num_active_neighbors(&cell, &active_cells, num_dimensions);
         if n == 2 || n == 3 {
-            new_cells.insert(cell);
+            new_cells.insert(cell.clone());
         }
     }
 
     // If a cube is inactive but exactly 3 of its neighbors are active, the cube becomes active.
     // Otherwise, the cube remains inactive.
-    for &cell in inactive_neighbors(&active_cells).iter() {
-        if num_active_neighbors(&cell, &active_cells) == 3 {
-            new_cells.insert(cell);
+    for cell in inactive_neighbors(&active_cells, num_dimensions).iter() {
+        if num_active_neighbors(&cell, &active_cells, num_dimensions) == 3 {
+            new_cells.insert(cell.clone());
         }
     }
 
     new_cells
 }
 
-fn inactive_neighbors(active_cells: &HashSet<Position>) -> Vec<Position> {
+fn inactive_neighbors(active_cells: &HashSet<Position>, num_dimensions: usize) -> Vec<Position> {
     let mut set = HashSet::new();
 
     for cell in active_cells {
-        for &neighbor in neighbors(&cell)
+        for neighbor in neighbors(&cell, num_dimensions)
             .iter()
             .filter(|&x| !active_cells.contains(x))
         {
-            set.insert(neighbor);
+            set.insert(neighbor.clone());
         }
     }
 
     set.into_iter().collect()
 }
 
-fn num_active_neighbors(position: &Position, active_cells: &HashSet<Position>) -> usize {
-    neighbors(&position)
+fn num_active_neighbors(
+    position: &Position,
+    active_cells: &HashSet<Position>,
+    num_dimensions: usize,
+) -> usize {
+    neighbors(&position, num_dimensions)
         .iter()
         .filter(|&x| active_cells.contains(x))
         .count()
 }
 
-fn neighbors(position: &Position) -> Vec<Position> {
+fn neighbors(position: &Position, num_dimensions: usize) -> Vec<Position> {
     let mut positions = Vec::new();
 
-    for x in &[position.x - 1, position.x, position.x + 1] {
-        for y in &[position.y - 1, position.y, position.y + 1] {
-            for z in &[position.z - 1, position.z, position.z + 1] {
-                if (x, y, z) != (&position.x, &position.y, &position.z) {
-                    positions.push(Position {
-                        x: *x,
-                        y: *y,
-                        z: *z,
-                    });
-                }
-            }
-        }
+    for displacement in displacements(num_dimensions) {
+        positions.push(
+            position
+                .iter()
+                .zip(displacement)
+                .map(|(a, b)| a + b)
+                .collect(),
+        );
     }
 
     positions
 }
 
-fn read_input(input: &str) -> HashSet<Position> {
+fn displacements(num_dimensions: usize) -> Vec<Position> {
+    let mut coordinates = add_coordinate(&vec![]);
+
+    for _ in 1..num_dimensions {
+        let mut new_coordinates: Vec<Position> = Vec::new();
+        for coordinate in coordinates {
+            for new_coordinate in add_coordinate(&coordinate) {
+                new_coordinates.push(new_coordinate);
+            }
+        }
+        coordinates = new_coordinates.clone();
+    }
+
+    // we need to remove 0, 0, ...  because this would not be a displacement
+    let null_vector: Vec<i32> = vec![0; num_dimensions];
+    let index = coordinates.iter().position(|x| x == &null_vector).unwrap();
+    coordinates.remove(index);
+
+    coordinates
+}
+
+fn add_coordinate(displacements: &Position) -> Vec<Position> {
+    let mut new_coordinates = Vec::new();
+
+    let mut new_vector = displacements.to_owned();
+    new_vector.push(-1);
+    new_coordinates.push(new_vector);
+
+    let mut new_vector = displacements.to_owned();
+    new_vector.push(0);
+    new_coordinates.push(new_vector);
+
+    let mut new_vector = displacements.to_owned();
+    new_vector.push(1);
+    new_coordinates.push(new_vector);
+
+    new_coordinates
+}
+
+fn read_input(input: &str, num_dimensions: usize) -> HashSet<Position> {
     let mut active_cells = HashSet::new();
 
     for (x, line) in input.lines().enumerate() {
         for (y, c) in line.chars().enumerate() {
             if c == '#' {
-                active_cells.insert(Position {
-                    x: x as i32,
-                    y: y as i32,
-                    z: 0,
-                });
+                let mut v = vec![0; num_dimensions];
+                v[0] = x as i32;
+                v[1] = y as i32;
+                active_cells.insert(v);
             }
         }
     }
