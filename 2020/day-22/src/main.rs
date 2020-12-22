c38b2f999b62246a15a8191d1d3da3e1b58ae9d6
Radovan Bast <bast@users.noreply.github.com>
2020-12-22 19:27:52 +0100

    rearrange

--- 2020/day-22/src/main.rs
+++ 2020/day-22/src/main.rs
@@ -4,49 +4,47 @@ use std::fs;
 fn main() {
     let input = fs::read_to_string("input.txt").unwrap();
 
-    let (mut cards_player1, mut cards_player2) = read_cards(&input);
+    let mut cards = read_cards(&input);
 
+    println!("result from part 1: {}", game(&mut cards));
+}
+
+fn game(cards: &mut [VecDeque<usize>]) -> usize {
     loop {
-        if cards_player1.is_empty() || cards_player2.is_empty() {
+        if cards[0].is_empty() || cards[1].is_empty() {
             break;
         }
-        let card1 = cards_player1.pop_front().unwrap();
-        let card2 = cards_player2.pop_front().unwrap();
+        let card1 = cards[0].pop_front().unwrap();
+        let card2 = cards[1].pop_front().unwrap();
         if card1 > card2 {
-            cards_player1.push_back(card1);
-            cards_player1.push_back(card2);
+            cards[0].push_back(card1);
+            cards[0].push_back(card2);
         } else {
-            cards_player2.push_back(card2);
-            cards_player2.push_back(card1);
+            cards[1].push_back(card2);
+            cards[1].push_back(card1);
         }
     }
 
-    println!(
-        "result from part 1: {}",
-        score(&cards_player1) + score(&cards_player2)
-    );
+    score(&cards[0]) + score(&cards[1])
 }
 
-fn score(cards: &VecDeque<usize>) -> usize {
+fn score(deck: &VecDeque<usize>) -> usize {
     let mut s = 0;
-    for (i, card) in cards.iter().rev().enumerate() {
+    for (i, card) in deck.iter().rev().enumerate() {
         s += (i + 1) * card;
     }
     s
 }
 
-fn read_cards(input: &str) -> (VecDeque<usize>, VecDeque<usize>) {
+fn read_cards(input: &str) -> Vec<VecDeque<usize>> {
+    let mut cards = Vec::new();
     let chunks: Vec<&str> = input.split("\n\n").collect();
-
-    let mut cards_player1 = VecDeque::new();
-    for line in chunks[0].lines().skip(1) {
-        cards_player1.push_back(line.parse().unwrap());
+    for chunk in chunks {
+        let mut q = VecDeque::new();
+        for line in chunk.lines().skip(1) {
+            q.push_back(line.parse().unwrap());
+        }
+        cards.push(q);
     }
-
-    let mut cards_player2 = VecDeque::new();
-    for line in chunks[1].lines().skip(1) {
-        cards_player2.push_back(line.parse().unwrap());
-    }
-
-    (cards_player1, cards_player2)
+    cards
 }
