61fa1858d1efbd0de81e57a98f41c220f5696247
Radovan Bast <bast@users.noreply.github.com>
2021-12-03 00:21:06 +0100

    more reusable text parsing

--- 2021/day-02/src/main.rs
+++ 2021/day-02/src/main.rs
@@ -1,22 +1,19 @@
 use std::fs;
 
-fn read_instructions(file_name: &str) -> Vec<(String, i32)> {
+fn parse_line(line: &str) -> (String, usize) {
+    let words: Vec<&str> = line.split_whitespace().collect();
+    let s = words[0].to_string();
+    let n: usize = words[1].parse().unwrap();
+    (s, n)
+}
+
+fn read_input(file_name: &str) -> Vec<(String, usize)> {
     let input = fs::read_to_string(file_name).unwrap();
-
-    let mut v = Vec::new();
-
-    for line in input.lines() {
-        let words: Vec<&str> = line.split_whitespace().collect();
-        let instruction = words[0].to_string();
-        let n: i32 = words[1].parse().unwrap();
-        v.push((instruction, n));
-    }
-
-    v
+    input.lines().map(|line| parse_line(line)).collect()
 }
 
 fn main() {
-    let instructions = read_instructions("input.txt");
+    let instructions = read_input("input.txt");
 
     let (mut x, mut y) = (0, 0);
     for (instruction, n) in &instructions {
