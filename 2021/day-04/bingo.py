74cc3c796a5dc5c93f5e81a4e9dc7a0614a5c4cd
Radovan Bast <bast@users.noreply.github.com>
2021-12-05 01:01:28 +0100

    simplify

--- 2021/day-04/bingo.py
+++ 2021/day-04/bingo.py
@@ -1,13 +1,11 @@
 from dataclasses import dataclass, field
-from collections import defaultdict
 
 
 @dataclass
 class Board:
     number_to_position: dict[int, (int, int)] = field(default_factory=dict)
-    position_is_drawn: dict[(int, int), bool] = field(
-        default_factory=lambda: defaultdict(bool)
-    )
+    drawn_positions: set[(int, int)] = field(default_factory=set)
+    has_won: bool = False
 
 
 def read_sequence(file_name: str) -> list[int]:
@@ -32,37 +30,35 @@ def read_boards(file_name: str) -> list[Board]:
                 return boards
 
 
-def get_unmarked_numbers(board: Board) -> list[int]:
+def sum_unmarked(board: Board) -> int:
     numbers = []
     for number, position in board.number_to_position.items():
-        if not board.position_is_drawn[position]:
+        if not position in board.drawn_positions:
             numbers.append(number)
-    return numbers
+    return sum(numbers)
 
 
 def board_wins(board: Board) -> bool:
     for i in range(5):
-        if all([board.position_is_drawn[(i, j)] for j in range(5)]):
+        if all([(i, j) in board.drawn_positions for j in range(5)]):
             return True
-        if all([board.position_is_drawn[(j, i)] for j in range(5)]):
+        if all([(j, i) in board.drawn_positions for j in range(5)]):
             return True
     return False
 
 
 def compute_scores(sequence, boards) -> list[int]:
-    boards_left = list(range(len(boards)))
     scores = []
     for n in sequence:
-        for m, board in enumerate(boards):
-            if m in boards_left:
+        for board in boards:
+            if not board.has_won:
                 if n in board.number_to_position:
                     i, j = board.number_to_position[n]
-                    board.position_is_drawn[(i, j)] = True
+                    board.drawn_positions.add((i, j))
                     if board_wins(board):
-                        scores.append(n * sum(get_unmarked_numbers(board)))
-                        boards_left.remove(m)
-                        if len(boards_left) == 0:
-                            return scores
+                        scores.append(n * sum_unmarked(board))
+                        board.has_won = True
+    return scores
 
 
 if __name__ == "__main__":
