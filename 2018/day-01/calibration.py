7ae8fed00c8365ec70907c74feb248bdb0cd7f47
Radovan Bast <bast@users.noreply.github.com>
2022-11-04 19:45:46 +0100

    refactor

--- 2018/day-01/calibration.py
+++ 2018/day-01/calibration.py
@@ -1,16 +1,18 @@
 from itertools import cycle
 
+
+def find_repeating(numbers: list[int]) -> int:
+    visited = set()
+    i = 0
+    for n in cycle(numbers):
+        i += n
+        if i in visited:
+            return i
+        else:
+            visited.add(i)
+
+
 numbers = list(map(int, open("input.txt", "r").read().splitlines()))
 
 print("part 1:", sum(numbers))
-
-visited = set()
-i = 0
-for n in cycle(numbers):
-    i += n
-    if i in visited:
-        break
-    else:
-        visited.add(i)
-
-print("part 2:", i)
+print("part 2:", find_repeating(numbers))
