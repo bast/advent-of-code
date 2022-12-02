060aaedb4a8dfaa74964b80508cc08799981e378
Radovan Bast <bast@users.noreply.github.com>
2022-12-02 08:25:38 +0100

    2022 day 2

---
+++ 2022/day-02/rps.py
@@ -0,0 +1,28 @@
+game1 = {
+    "A X": 3 + 1,
+    "A Y": 6 + 2,
+    "A Z": 0 + 3,
+    "B X": 0 + 1,
+    "B Y": 3 + 2,
+    "B Z": 6 + 3,
+    "C X": 6 + 1,
+    "C Y": 0 + 2,
+    "C Z": 3 + 3,
+}
+
+game2 = {
+    "A X": 0 + 3,
+    "A Y": 3 + 1,
+    "A Z": 6 + 2,
+    "B X": 0 + 1,
+    "B Y": 3 + 2,
+    "B Z": 6 + 3,
+    "C X": 0 + 2,
+    "C Y": 3 + 3,
+    "C Z": 6 + 1,
+}
+
+lines = open("input.txt", "r").read().splitlines()
+
+print("part 1:", sum(map(lambda l: game1[l], lines)))
+print("part 2:", sum(map(lambda l: game2[l], lines)))
